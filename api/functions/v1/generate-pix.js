7f4d6132d771f2605e4112eb24d499461f53c305
peagasfx <pedrogcubes@gmail.com>
2026-02-11 14:58:50 -0300

    Fix CORS header for PIX API handler

--- api/functions/v1/generate-pix.js
+++ api/functions/v1/generate-pix.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   // =========================
   // 🔥 CORS (OBRIGATÓRIO)
   // =========================
-  res.setHeader("Access-Control-Allow-Origin", "https://upssel-nine.vercel.app/");
+  res.setHeader("Access-Control-Allow-Origin", "https://upssel-nine.vercel.app");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
@@ -62,5 +62,6 @@ export default async function handler(req, res) {
     });
   }
 }
+
 
 
